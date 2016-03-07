87f6611a54d3be405055074cc410b3ccbf57dd91
Robert de Bock <robert@meinit.nl>
2016-03-08 00:56:08 +0100

    Update createrepo.sh
    removed a x86_64 option.

--- createrepo.sh
+++ createrepo.sh
@@ -2,7 +2,6 @@
 
 yum -y install createrepo
 mkdir /data/repository/
-cp /data/rpmbuild/RPMS/x86_64/* /data/repository/
 cp /data/rpmbuild/RPMS/noarch/* /data/repository/
 cd /data/repository/
 createrepo .
